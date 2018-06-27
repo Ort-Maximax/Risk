ba33d63e6918ebdd20ce1c0a592561e07811d097
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-06-27 14:16:00 +0200

    Removed useless import

--- risk-ws/src/main/java/com/ort/risk/ws/config/ContextConfig.java
+++ risk-ws/src/main/java/com/ort/risk/ws/config/ContextConfig.java
@@ -1,17 +1,12 @@
 package com.ort.risk.ws.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 @Configuration
 @ComponentScan(value = {"com.ort.risk.ws.repository"})
 public class ContextConfig {
 
-	// TODO : What datasource?
+	
 	
 }
