60401bfc9969039459c999759062795c688c934a
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-06-27 14:15:21 +0200

    Data transfert object for players

--- risk-ws/src/main/java/com/ort/risk/ws/dto/PlayerDTO.java
+++ risk-ws/src/main/java/com/ort/risk/ws/dto/PlayerDTO.java
@@ -2,4 +2,27 @@ package com.ort.risk.ws.dto;
 
 public class PlayerDTO {
 
+	private String name;
+    private int order;
+    private int nbTroops;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getOrder() {
+		return order;
+	}
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	public int getNbTroops() {
+		return nbTroops;
+	}
+	public void setNbTroops(int nbTroops) {
+		this.nbTroops = nbTroops;
+	}
+		
 }
