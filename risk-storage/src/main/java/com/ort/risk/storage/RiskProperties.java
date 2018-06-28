19e97dbd1dc5ada864885ae06034eb6b2dcff4e1
Thibaut Moiroud <thib.moiroud@gmail.com>
2018-06-28 15:45:59 +0200

    Adding logger

--- risk-storage/src/main/java/com/ort/risk/storage/RiskProperties.java
+++ risk-storage/src/main/java/com/ort/risk/storage/RiskProperties.java
@@ -4,16 +4,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * 
  * @author tibo
  *
- * Application's properties --> config.properties
+ *         Application's properties --> config.properties
  */
 public class RiskProperties {
+
+	private static final Logger logger = LogManager.getLogger(RiskProperties.class);
 	
 	private static Properties prop = new Properties();
-	
+	private static RiskProperties INSTANCE = null;
+
 	public static final String SAVED_MAP_PATH_PROP = "savedMapPath";
 	public static final String CURRENT_MAP_PATH_PROP = "currentMapPath";
 
@@ -21,27 +27,33 @@ public class RiskProperties {
 		FileInputStream input = null;
 		try {
 			input = new FileInputStream("resources/config.properties");
+			logger.info("Loading the properties' file.");
 			prop.load(input);
-		} catch (IOException io) {
-			io.printStackTrace();
+		} catch (IOException e) {
+			logger.fatal("Error while accessing to the properties' file : {}", () -> e.getMessage());
 		} finally {
 			if (input != null) {
 				try {
 					input.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					logger.fatal("Error while closing the properties file's stream : {}", () -> e.getMessage());
 				}
 			}
 
 		}
 	}
-	
+
 	public static RiskProperties getInstance() {
-		return new RiskProperties();
+		if (INSTANCE == null) {
+			INSTANCE = new RiskProperties();
+			logger.info("Initating RiskProperties Singleton.");
+		}
+		return INSTANCE;
 	}
-	
+
 	public String getProperty(String key) {
+		logger.info("Getting property : {}", key);
 		return prop.getProperty(key);
 	}
-	
+
 }
--- risk-ws/src/main/java/com/ort/risk/ws/web/controller/MapController.java
+++ risk-ws/src/main/java/com/ort/risk/ws/web/controller/MapController.java
@@ -4,22 +4,23 @@ import com.ort.risk.model.Map;
 import com.ort.risk.storage.RiskProperties;
 import com.ort.risk.ws.dto.MapDTO;
 import com.ort.risk.ws.repository.MapRepository;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/maps")
 public class MapController {
 
+	private static final Logger logger = LogManager.getLogger(MapController.class);
     RiskProperties properties = RiskProperties.getInstance();
 
     @Resource
@@ -27,62 +28,83 @@ public class MapController {
 
     @GetMapping
     public List<Map> getMaps() {
+    	logger.info("getMaps() : Getting every maps.");
         return mapRepository.getAll();
     }
 
     @PostMapping
     public boolean createMap(@RequestBody MapDTO map) {
-        if (map == null)
-            return false;
+        if (map == null) {
+        	logger.error("createMap() : No data in the request body.");
+        	return false;
+        }
 
         //https://raw.githubusercontent.com/EISAWESOME/orbit-360/master/404.html
-
         File file = new File(properties.getProperty(RiskProperties.SAVED_MAP_PATH_PROP) + map.getName());
-
 
         try {
             FileUtils.copyURLToFile(new URL(map.getDataUrl()), file);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.fatal("createMap() : Error while copying the map file -> {}", () -> e.getMessage());
+            return false;
         }
 
         Map mapToInsert = new Map();
         // TODO : transfer data from  object to map
 
         // mapRepository.create(mapFound);
+        logger.info("createMap() : Creating a map.");
         return true;
     }
 
     @GetMapping(value="/{id}")
-    public Map getMap(@PathVariable Long id) {
-        return mapRepository.get(id);
+    public MapDTO getMap(@PathVariable Long id) {
+    	Map map = mapRepository.get(id);
+    	MapDTO mapToReturn = null;
+    	
+    	if (map == null) {
+    		logger.info("getMap() : No map found with id {}", id);
+    		return null;
+    	} else {
+    		mapToReturn = new MapDTO();
+    		// TODO : transfer data of map to mapToReturn
+    		logger.info("getMap() : Getting the map {}", id);
+    	}
+    	return mapToReturn;
     }
 
 
     @PutMapping(value="/{id}")
-    public boolean updateMap(@PathVariable Long id, @RequestBody Map map) {
-        if (map == null)
-            return false;
+    public boolean updateMap(@PathVariable Long id, @RequestBody MapDTO map) {
+    	if (map == null) {
+        	logger.error("updateMap() : No data in the request body.");
+        	return false;
+        }
 
         Map mapFound = mapRepository.get(id);
-        if (mapFound == null)
-            return false;
-        else {
+        if (mapFound == null) {
+        	logger.info("updateMap() : No map found with id {}", id);
+        	return false;
+        } else {
+        	Map mapToUpdate = new Map();
             // TODO : transfer data from  object to map
-            mapRepository.update(map);
+            mapRepository.update(mapToUpdate);
+            logger.info("updateMap() : Updating the map {}", id);
             return true;
         }
     }
 
     @DeleteMapping(value="/{id}")
     public boolean deleteMap(@PathVariable Long id) {
-
         Map mapFound = mapRepository.get(id);
-        if (mapFound == null)
-            return false;
+        if (mapFound == null) {
+        	logger.info("deleteMap() : No map found with id {}", id);
+        	return false;
+        }
         else {
             // TODO : transfer data from  object to map
             mapRepository.delete(mapFound);
+            logger.info("deleteMap() : Deleting the map {}", id);
             return true;
         }
     }
